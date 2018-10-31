e3abe0e3378f95156b673e851c6f80caac32f279
TheoObbard <theo@dali.dartmouth.edu>
2018-10-31 14:14:18 -0700

    Add #transpose and #stock_picker methods and corresponding rspecs

--- tdd/lib/exercise.rb
+++ tdd/lib/exercise.rb
@@ -24,3 +24,42 @@ class Array
   end
 
 end
+
+# matrix = 2D array
+def my_transpose(matrix)
+  raise ArgumentError unless matrix.is_a?(Array)
+  matrix.transpose
+end
+
+def stock_picker(prices_per_day)
+  return [] if prices_per_day.sort.reverse == prices_per_day
+
+  i = 0
+  result = []
+  while i < prices_per_day.length-1
+    j = i + 1
+    while j < prices_per_day.length
+      result << [prices_per_day[i], prices_per_day[j]]
+      j += 1
+    end
+    i += 1
+  end
+
+  highest_pair = [0, 0]
+  result.each do |pair|
+    if highest_pair[1] - highest_pair[0] < pair[1] - pair[0]
+      highest_pair = pair
+    end
+  end
+
+  index_pair = []
+  prices_per_day.each_with_index do |price, i|
+    if price == highest_pair[0]
+      index_pair << i
+    elsif price == highest_pair[1] && index_pair.length == 1
+      index_pair << i
+      # index_pair[1] = i
+    end
+  end
+  index_pair
+end
--- tdd/spec/exercise_spec.rb
+++ tdd/spec/exercise_spec.rb
@@ -29,3 +29,33 @@ RSpec.describe Array do
   end
 
 end
+
+RSpec.describe do
+  describe "#my_transpose" do
+    # it "takes an array" do
+    #   expect{my_transpose([])}.to_not raise_error(ArgumentError)
+    # end
+
+    it "returns an array" do
+      expect(my_transpose([])).to eq([])
+    end
+
+    it "transposes the given matrix" do
+      expect(my_transpose([[0, 1, 2],[3, 4, 5],[6, 7, 8]])).to eq([[0, 3, 6],[1, 4, 7],[2, 5, 8]])
+    end
+  end
+
+  describe "#stock_picker" do
+    it "takes an array of prices each day" do
+      expect{stock_picker([1, 2, "three"])}.to raise_error(ArgumentError)
+    end
+
+    it "outputs the most profitable pair of days" do
+      expect(stock_picker([10, 30, 50, 70, 5, 40])).to eq([0, 3])
+    end
+
+    it "returns no pair if you can't make a profit" do
+      expect(stock_picker([6, 5, 4, 3, 2, 1])).to eq([])
+    end
+  end
+end
